dcb826483493dfd21e534a8f45c0e367ca181027
Miquel De Cáceres <miquelcaceres@gmail.com>
2023-04-29 17:32:49 +0200

    update paths

--- R/site_data_path_functions.R
+++ R/site_data_path_functions.R
@@ -1,6 +1,6 @@
 pkg.globals <- new.env()
 
-pkg.globals$site_data_path <- "/home/miquel/OneDrive/Professional/MEDFATE_development/StandLevelEvaluation/"
+pkg.globals$site_data_path <- "/home/miquel/OneDrive/mcaceres_work/model_development/medfate_development/StandLevelEvaluation/"
 
 #' Site data path
 #'
